ca13159e555c44ac7d6d9d81fc7b13c313b319f4
Prasanna <vsjs.prasanna@gmail.com>
2018-10-28 10:43:33 +0800

    Populate Genre Table

---
+++ GigHubCore/Migrations/20181028021403_PopulateGenres.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GigHubCore.Migrations
+{
+    public partial class PopulateGenres : Migration
+    {
+        private string[] columns = new[] { "Id", "Name" };
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Genres", columns, new object[]{1,"Jazz"});
+            migrationBuilder.InsertData("Genres", columns, new object[] { 2, "Blues" });
+            migrationBuilder.InsertData("Genres", columns, new object[] { 3, "Rock" });
+            migrationBuilder.InsertData("Genres", columns, new object[] { 4, "Country" });
+        }         
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("Genres", "Id", 1);
+            migrationBuilder.DeleteData("Genres", "Id", 2);
+            migrationBuilder.DeleteData("Genres", "Id", 3);
+            migrationBuilder.DeleteData("Genres", "Id", 4);
+        }
+    }
+}
