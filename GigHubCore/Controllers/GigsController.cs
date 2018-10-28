97805a9aa49888f4556a0fcd38d243da4a738718
Prasanna <vsjs.prasanna@gmail.com>
2018-10-28 18:27:32 +0800

    Create Add a Gig form

---
+++ GigHubCore/Controllers/GigsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GigHubCore.Data;
+using GigHubCore.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GigHubCore.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly DataContext _context;
+      
+        public GigsController()
+        {
+            _context = new DataContext();
+        }
+
+        public IActionResult Create()
+        {
+            var gigViewModel = new GigViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(gigViewModel);
+        }
+    }
+}
---
+++ GigHubCore/ViewModels/GigViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GigHubCore.Models;
+
+namespace GigHubCore.ViewModels
+{
+    public class GigViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
